d659e117bfee67ea828ce7d6cfcc78252a909ff4
remco van der Velde <remco.pc@outlook.com>
2023-12-28 16:31:46 +0100

    apache2 basic

---
+++ src/Package/Trait/Configure.php
@@ -0,0 +1,51 @@
+<?php
+namespace Package\R3m\Io\Apache2\Trait;
+
+use R3m\Io\App;
+
+use R3m\Io\Module\Dir;
+use R3m\Io\Module\Core;
+use R3m\Io\Module\File;
+
+use R3m\Io\Node\Model\Node;
+
+use Exception;
+trait Configure {
+
+    public function apache2(): void
+    {
+        $object = $this->object();
+        if($object->config(Config::POSIX_ID) !== 0){
+            return;
+        }
+        //php and apache2 should be installed by docker.
+        //if there is a different sury package, there are multiple versions
+
+        $dir = new Dir();
+        $read = $dir->read('/etc/php/');
+
+        ddd($read);
+
+        Dir::create('/run/php');
+        $command = 'a2enmod proxy_fcgi setenvif';
+        exec($command);
+        $command = 'a2enconf ' . PHP::FPM;
+        exec($command);
+        $command = 'a2dismod php8.2';
+        exec($command);
+        $command = 'a2dismod mpm_prefork';
+        exec($command);
+        $command = 'a2enmod mpm_event';
+        exec($command);
+        $command = 'a2enmod http2';
+        exec($command);
+        $command = 'a2enmod rewrite';
+        exec($command);
+        $command = 'a2enmod ssl';
+        exec($command);
+        $command = 'a2enmod md';
+        exec($command);
+        $command = '. /etc/apache2/envvars';
+        exec($command);
+    }
+}
--- src/Package/View/Setup/Setup.tpl
+++ src/Package/View/Setup/Setup.tpl
@@ -2,4 +2,5 @@
 {{$register = Package.R3m.Io.Apache2:Init:register()}}
 {{if(!is.empty($register))}}
 {{Package.R3m.Io.Apache2:Import:role.system()}}
+{{Package.R3m.Io.Apache2:Configure:apache2()}}
 {{/if}}
